87957c774d9af38ed5b62e8be65c929369578f32
Svencodeflow <schlaakboot@gmail.com>
2023-03-03 16:30:58 +0100

    Designer_shop

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+console.log("hello")
+
+const val = document.getElementById('vname')
+const val2 = document.getElementById('nname')
+const val3 = document.getElementById('email')
+
+function daten() {
+    console.log("Vorname: "+val.value);
+    console.log("Nachname: "+val2.value);
+    console.log("Email: "+ val3.value);
+}
